9ff6444bb52bf4dcbf2406f64464968d9f06a8b1
Flaze07 <Flaze07@users.noreply.github.com>
2024-08-07 09:25:39 +0700

    Created Request class to handle Request data

---
+++ src/main/java/io/github/flaze07/net/Request.java
@@ -0,0 +1,47 @@
+package io.github.flaze07.net;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class Request
+{
+	private final String path;
+	private final Map<String, String> queries;
+
+	public Request(Builder builder) {
+		this.path = builder.path;
+		this.queries = builder.queries;
+	}
+
+	public String getpath() {
+		return path;
+	}
+
+	public Optional<String> getQuery(String key) {
+		if(!queries.containsKey(key)) {
+			return Optional.empty();
+		} else {
+			return Optional.of(queries.get(key));
+		}
+	}
+
+	public static class Builder 
+	{
+		private String path;
+		private Map<String, String> queries;
+
+		public Builder path(String path) {
+			this.path = path;
+			return this;
+		}
+
+		public Builder queries(Map<String, String> queries) {
+			this.queries = queries;
+			return this;
+		}
+
+		public Request build() {
+			return new Request(this);
+		}
+	}
+}
