826046d8499f713b471c1485da7ec9558d3543f0
Flaze07 <Flaze07@users.noreply.github.com>
2024-08-07 09:26:17 +0700

    Created StringFunctions utility

---
+++ src/main/java/io/github/flaze07/util/StringFunctions.java
@@ -0,0 +1,24 @@
+package io.github.flaze07.util;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StringFunctions 
+{
+	public static Map<String, String> splitQuery(String queries) {
+		if (queries == null || "".equals(queries)) {
+			return Collections.emptyMap();
+		}
+
+		var result = new HashMap<String, String>();
+
+		String[] queryList = queries.split("&");
+		for(int i = 0; i < queryList.length; ++i) {
+			String[] keyValuePair = queryList[0].split("=");
+			result.put(keyValuePair[0], keyValuePair[1]);
+		}
+
+		return result;
+	}
+}
